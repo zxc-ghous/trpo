808f3f864bc5fdf0ef0848fccde99c01807ee589
David <davidtrufanov@mail.ru>
2023-05-17 12:00:38 +0700

    add uml diagram

--- FileObserver/main.cpp
+++ FileObserver/main.cpp
@@ -10,10 +10,10 @@ int main(int argc, char *argv[])
 
     QCoreApplication a(argc, argv);
     FileObserver& file_watcher = FileObserver::instance();
-    QTimer *timer = new QTimer();
+    QTimer timer = QTimer();
 
     QObject::connect(&file_watcher,&FileObserver::logMessage,&FileObserver::fileMessage);
-    QObject::connect(timer, &QTimer::timeout,&file_watcher,&FileObserver::fileChecker);
+    QObject::connect(&timer, &QTimer::timeout,&file_watcher,&FileObserver::fileChecker);
 
 //    file_watcher.addFile("F:\\my_datasets\\lab1Testing\\test1.txt");
 //    file_watcher.addFile("F:\\my_datasets\\lab1Testing\\test2.txt");
@@ -21,12 +21,12 @@ int main(int argc, char *argv[])
 
     file_watcher.setDirectory("F:\\my_datasets\\lab1Testing");
 
-    QFileInfo info1("F:\\my_datasets\\lab1Testing\\test1.txt");
-    qDebug()<<info1.absoluteFilePath();
-    qDebug()<<info1.size();
+//    QFileInfo info1("F:\\my_datasets\\lab1Testing\\test1.txt");
+//    qDebug()<<info1.absoluteFilePath();
+//    qDebug()<<info1.size();
 
 
-    timer->start(1000);
+    timer.start(1000);
     return a.exec();
 }
 
