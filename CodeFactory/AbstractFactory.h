d3d67ab0f2935613abd0fc04a5ee70f74ae09003
David <davidtrufanov@mail.ru>
2023-08-10 18:46:31 +1000

    add abstract factory

---
+++ CodeFactory/AbstractFactory.h
@@ -0,0 +1,18 @@
+
+#ifndef ABSTRACTFACTORY_H
+#define ABSTRACTFACTORY_H
+
+
+class CodeFactory
+{
+public:
+    virtual ~CodeFactory() = default;
+    using Flags = unsigned int;
+
+    virtual std::shared_ptr<MethodUnit> createMethodUnit(const std::string& name, const std::string& returnType, Flags flags) const = 0;
+    virtual std::shared_ptr<ClassUnit> createClassUnit(const std::string& name) const = 0;
+    virtual std::shared_ptr<PrintOperatorUnit> createPrintOperatorUnit(const std::string& text) const = 0;
+};
+
+
+#endif // ABSTRACTFACTORY_H
